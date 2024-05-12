b6eb526e7f45c32463dc8f0eda4423a7b6e6f54e
Clayman <Clayman@DESKTOP-DM8E77U>
2024-05-12 16:23:19 +0300

    Fraction start

---
+++ IntroductionToOOP/Fraction/main.cpp
@@ -0,0 +1,125 @@
+﻿//Fraction
+#include<iostream>
+using namespace std;
+
+class Fraction
+{
+	int integer;	//целое
+	int numerator;	//числитель
+	int denominator;//знаменатель
+public:
+	int get_integer()const
+	{
+		return integer;
+	}
+	int get_numerator()const
+	{
+		return numerator;
+	}
+	int get_denominator()const
+	{
+		return denominator;
+	}
+	void set_integer(int integer)
+	{
+		this->integer = integer;
+	}
+	void set_numerator(int numerator)
+	{
+		this->numerator = numerator;
+	}
+	void set_denominator(int denominator)
+	{
+		if (denominator == 0)denominator = 1;
+		this->denominator = denominator;
+	}
+
+	//			Constructors:
+	Fraction()
+	{
+		integer = 0;
+		numerator = 0;
+		denominator = 1;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Fraction(int integer)
+	{
+		this->integer = integer;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		set_numerator(numerator);
+		set_denominator(denominator);
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Fraction(int integer, int numerator, int denominator)
+	{
+		set_integer(integer);
+		set_numerator(numerator);
+		set_denominator(denominator);
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Fraction()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//			Operators:
+	Fraction& operator=(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+
+	//			Methods:
+	void print()const
+	{
+		if (integer)cout << integer;
+		if (numerator)
+		{
+			if (integer)cout << "(";
+			cout << numerator << "/" << denominator;
+			if (integer)cout << ")";
+		}
+		else if (integer == 0)cout << 0;
+		cout << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	
+	Fraction A;		//Default constructor
+	A.print();
+
+	Fraction B = 5;	//Single-argument constructor
+	B.print();
+
+	Fraction C(1, 2);
+	C.print();
+
+	Fraction D(2, 3, 4);
+	D.print();
+
+	Fraction E = D;	//Copy constructor
+	E.print();
+
+	Fraction F;
+	F = E;			//Copy assignment
+	F.print();
+}
--- ReadMe.txt
+++ ReadMe.txt
@@ -3,6 +3,15 @@ https://www.youtube.com/playlist?list=PLeqyOOqxeiIPsB74OvAMwaJ19mKMGE6h2
 https://chat.whatsapp.com/KclR3Z42AVuFzvOw58F5Se
 
 TODO:
+Для класса 'Fraction' перегрузить следующие операторы:
+	1. Арифметические опервторы: +, -, *, /;
+	2. Increment/Decrement:		 ++/--;
+	3. Compound assignments:	 +=, -=, *=, /=;
+	4. Операторы сравнения:		 ==, !=, >, <, >=, <=;
+	5. Операторы перенаправления в поток 
+	   и изъятия из потока, для работы cin/cout;
+
+DONE:
 0. Выучить теорию!!!!!!!!
 1. В проекте 'IntroductionToOOP' написать метод ??? distance(???)
    который возвращает расстояние до указанной точки;
